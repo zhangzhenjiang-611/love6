931125ae0d66b6f7d68dbd9c7ee77851072bca55
zhangzhenjiang <1569177080@qq.com>
2019-12-06 00:00:59 +0800

    update

---
+++ php_singnel_demo/default_params.php
@@ -0,0 +1,26 @@
+<?php
+//默认参数的函数
+//error_reporting(E_ALL);
+function demo($name='yanli',$age=18,$sex='girl',$email='156@qq.com'){
+    echo "{$name}--------{$age}---------{$sex}-------{$email} <br/>";
+    //echo $name;
+}
+
+//demo("meizi",18,'girl','123@qq.com','11'); //实参个数多于形参，不会报错，少于会报错
+
+demo(); //在形参里加默认值，防止报错
+demo('xiaojiang');
+demo('xiaojiang',19);
+demo('xiaojiang',19,'boy');
+demo('xiaojiang',19,'boy','520@qq.com');
+
+
+function demo1($name,$age,$sex='girl',$email='156@qq.com'){
+    echo "{$name}--------{$age}---------{$sex}-------{$email} <br/>";
+    //echo $name;
+}
+//可以 参数中一部分给默认值，但是没有默认值的形参必须放在前面
+demo1('liuyanli',22);
+demo1('liuyanli',22,'girls');
+demo1('liuyanli',22,'girls','521@qq.com');
+//demo1('liuyanli');  //fatal error
