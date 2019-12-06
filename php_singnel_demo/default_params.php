c31aeeb4d0a0a728ca904d95e43c224c968e6d7d
zhangzhenjiang <zhangzhenjiang@xiaohe.com>
2019-12-06 10:00:54 +0800

    update

--- php_singnel_demo/default_params.php
+++ php_singnel_demo/default_params.php
@@ -6,7 +6,7 @@ function demo($name='yanli',$age=18,$sex='girl',$email='156@qq.com'){
     //echo $name;
 }
 
-//demo("meizi",18,'girl','123@qq.com','11'); //实参个数多于形参，不会报错，少于会报错
+demo("meizi",18,'girl','123@qq.com','11'); //实参个数多于形参，不会报错，少于会报错
 
 demo(); //在形参里加默认值，防止报错
 demo('xiaojiang');
@@ -30,9 +30,9 @@ demo1('liuyanli',22,'girls','521@qq.com');
 //可变参数个数的函数
 function demo2(){
     echo "###############<br>";
-    $arr = func_get_args();  //获取外部所有的参数
+    $arr = func_get_args();  //获取外部所有的参数 返回数组
     var_dump($arr);
-    $num = func_num_args(); //获取外部所有的参数的个数
+    $num = func_num_args(); //获取外部所有的参数的个数 返回int
     var_dump($num);
 
     $zhiding = func_get_arg(1);   //获取外部指定的参数
--- php_singnel_demo/huidiao_func.php
+++ php_singnel_demo/huidiao_func.php
@@ -6,3 +6,40 @@
  * Time: 0:49
  */
 //回调函数
+
+ $arr = [
+     'aaaaaaa','bb','cccccccccccccc','d','eeeeeeee','fffffffffffffffff','gg'
+ ];
+ usort($arr,'sortLength'); //按字符串的长度从小到大排序
+ var_dump($arr);
+/*function sortLength($a,$b){
+    if(strlen($a) > strlen($b)){
+        return 1;
+    }else if(strlen($a) < strlen($b)){
+        return -1;
+    }else{
+        return 0;
+    }
+}*/
+
+function sortLength($a,$b){
+    return strlen($a) > strlen($b) ? 1 : -1;
+}
+ //var_dump($arr);
+
+function Compare($str1, $str2) {
+         if (($str1 % 2 == 0) && ($str2 %2 == 0)) {
+                 if ($str1 > $str2)
+                         return - 1;
+          else
+              return 1;
+      }
+      if ($str1 % 2 == 0)
+                 return 1;
+     if ($str2 % 2 == 0)
+                return -1;
+     return ($str2 > $str1) ? 1 : - 1;
+ }
+ $scores = array (22,57,55,12,87,56,54,11);
+ usort ( $scores, 'Compare' );
+ print_r ( $scores );
