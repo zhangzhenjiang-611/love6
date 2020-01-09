d95651b5febf39bb5566a8dda27da1fd5dbda59a
zhangzhenjiang <zhangzhenjiang@xiaohe.com>
2020-01-09 19:53:40 +0800

    aa

---
+++ php_singnel_demo/bibao1.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 15691
+ * Date: 2020/1/9
+ * Time: 19:18
+ */
+function demo($func){
+    echo $func();
+}
+demo(function(){
+    return 'aaaaaaaaa';
+});
